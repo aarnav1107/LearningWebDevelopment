47454f7033ea92118623249a9f4407f0aa074b7a
aarnav1107 <110121330+aarnav1107@users.noreply.github.com>
2024-01-18 23:32:05 +0530

    Add files via upload

---
+++ webdev2024/Javascript/Lecture3/script.js
@@ -0,0 +1,12 @@
+const firstno=2;
+const secondno=5;
+// const z=firstno+secondno
+
+const z= firstno** secondno;
+console.log(z);
+
+// Assignment opetator
+let c= 5;
+c +=10 ; 
+// it is equal to c=c+10
+console.log(c);
