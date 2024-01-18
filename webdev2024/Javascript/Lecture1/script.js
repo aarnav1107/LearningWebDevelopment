1e4dd372d4d088f58b8890032f6c40c3fe61e06a
aarnav1107 <110121330+aarnav1107@users.noreply.github.com>
2024-01-18 23:30:35 +0530

    Add files via upload

---
+++ webdev2024/Javascript/Lecture1/script.js
@@ -0,0 +1,16 @@
+let c=12;
+let d=10;
+console.log("c+d is", c+d);
+let age = 26; // initialisation + declaration
+
+const name = 'Aarnav';
+const Name = "aryan";
+
+// console.log('age', age)
+// age = 28;
+
+// console.log("c+d is", c + d);
+
+// console.log('age', age)
+console.log('name', name);
+console.log('name', Name);
