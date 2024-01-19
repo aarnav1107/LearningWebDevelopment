7661e1eb738022d63e4a2b30fff0ec2ce7308fd8
aarnav1107 <110121330+aarnav1107@users.noreply.github.com>
2024-01-19 23:18:11 +0530

    Add files via upload

---
+++ webdev2024/Javascript/Lecture7/script.js
@@ -0,0 +1,43 @@
+const words=['marr','aa','dbdg','ggs'];
+// console.log(words);
+// console.log(words[1]);
+// console.log(words.length);
+// const spell=words;
+// spell[1]="vijay";
+// console.log(words);
+// console.log(spell);
+words.sort();
+console.log(words);
+
+
+const nums = [4, 1, 2, 8, 2, 8, 6];
+
+// console.log(nums.indexOf(80));
+// console.log(nums.includes(80));
+
+// nums.sort();
+
+const array = nums.slice(2, 4);
+
+console.log(array);
+
+// const words = [
+//     "Apple",
+//     "Banana",
+//     "Cherry",
+//     12,
+//     true,
+//     {
+//         name: "Anuj",
+//     },
+//     function hello() {
+//         console.log("hello world");
+//     },
+// ];
+
+// const newWords = words;
+
+// newWords[6] = "Kiwi";
+
+// console.log(words);
+// console.log(newWords);
